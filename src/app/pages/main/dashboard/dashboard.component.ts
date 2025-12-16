7c967a4796ab7352145b015ec67b714016b41de2
learningjsfor6m-ui <learningjsfor6m@gmail.com>
2025-12-16 19:49:02 +0530

    #18 commit for 1.Sorting and layout changes

--- src/app/pages/main/dashboard/dashboard.component.ts
+++ src/app/pages/main/dashboard/dashboard.component.ts
@@ -1,12 +1,19 @@
-import { Component, ElementRef, ViewChild, afterNextRender } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  ViewChild,
+  afterNextRender,
+} from '@angular/core';
 import { GodownServiceService } from '../../../core/godown-service.service';
 import { GodownDetails } from '../../../shared/interfaces/godown-details/godown.interface';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { FilterPipe } from '../../../shared/pipes/filter.pipe';
-import { FilterHeaderComponent } from "../../shared/filter-header/filter-header.component";
-import { DynamicFormComponent } from "../../shared/dynamic-form/dynamic-form.component";
+import { FilterHeaderComponent } from '../../shared/filter-header/filter-header.component';
+import { DynamicFormComponent } from '../../shared/dynamic-form/dynamic-form.component';
+import { formConfig } from '../../shared/config';
+import { IForm } from '../../../shared/interfaces/dynamic-form/form.interface';
 
 // import { OnpushDemoComponent } from "../demo/onpush-demo/onpush-demo.component";
 
@@ -14,59 +21,97 @@ import { DynamicFormComponent } from "../../shared/dynamic-form/dynamic-form.com
   selector: 'dashboard',
   imports: [CommonModule, FormsModule, FilterPipe, FilterHeaderComponent],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrl: './dashboard.component.scss',
 })
 export class DashboardComponent {
-  godownDetails:GodownDetails[]=[]
-  isLoading:boolean = false
-  headingOfPage:string = 'Turmeric Godown Dashboard'
-  searchText:string = ''
-  selectedItem:string = ''
-  isLayout:string ='card'
-  @ViewChild('username') inputRef!: ElementRef<HTMLInputElement>
+  godownDetails: GodownDetails[] = [];
+  isLoading: boolean = false;
+  headingOfPage: string = 'Turmeric Godown Dashboard';
+  searchText: string = '';
+  selectedItem: string = '';
+  isLayout: string = 'card';
+  @ViewChild('username') inputRef!: ElementRef<HTMLInputElement>;
+  sortOptions: any;
+  addNewGodown = formConfig as IForm;
 
-constructor(private godownService:GodownServiceService,private router:Router){
+  constructor(
+    private godownService: GodownServiceService,
+    private router: Router
+  ) {
+    this.sortOptions = [
+      { id: 0, label: 'Name Ascending', column: 'name', direction: 'asc' },
+      { id: 1, label: 'Name Descending', column: 'name', direction: 'desc' },
+      { id: 2, label: 'Status Ascending', column: 'status', direction: 'asc' },
+      {
+        id: 3,
+        label: 'Status Descending',
+        column: 'status',
+        direction: 'desc',
+      },
+      {
+        id: 4,
+        label: 'Location Ascending',
+        column: 'location',
+        direction: 'asc',
+      },
+      {
+        id: 5,
+        label: 'Location Descending',
+        column: 'location',
+        direction: 'desc',
+      },
+    ];
+  }
 
-}
+  ngOnInit() {
+    this.isLoading = true;
+    setTimeout(() => {
+      this.godownService.getGodowndetails().subscribe(
+        (res: GodownDetails[]) => {
+          this.godownDetails = res;
+          console.log(res);
+          this.isLoading = false;
+        },
+        (error) => {
+          this.isLoading = false;
+        }
+      );
+    }, 1000);
+  }
 
-ngOnInit(){
-  this.isLoading =true
-  setTimeout(()=>{
-    this.godownService.getGodowndetails().subscribe((res:GodownDetails[])=>{
-    this.godownDetails= res
-    console.log(res)
-     this.isLoading =false
-  },error=>{
+  // New Lifcycle hooks in 17,18
+  // afterRender(() => {
+  //   console.log('DOM is ready!')
+  // })
+  // afterNextRender(() => {
+  //   console.log('View is updated after state change')
+  // });
+  gotoGodownPage(godown: GodownDetails) {
+    console.log(godown);
+    let godownId: number = godown.id;
+    this.router.navigate(['/layout/stock-report', godownId]);
+  }
 
-     this.isLoading =false
-  })
-  },1000)
-}
+  getText($event: string) {
+    this.searchText = $event;
+  }
+  getLayoutFlag(event: any) {
+    console.log(event);
+    this.isLayout = event;
+  }
+  onSort(sort: any) {
+    const { key, direction } = sort;
 
-// New Lifcycle hooks in 17,18
-// afterRender(() => {
-//   console.log('DOM is ready!')
-// })
-// afterNextRender(() => {
-//   console.log('View is updated after state change')
-// });
-gotoGodownPage(godown:GodownDetails){
-console.log(godown)
-let godownId:number = godown.id
-this.router.navigate(['/layout/stock-report', godownId]);
-}
+    this.godownDetails = [...this.godownDetails].sort((a: any, b: any) => {
+      // Handle undefined / optional values
+      const aValue = a[key] ?? '';
+      const bValue = b[key] ?? '';
 
-getText($event:string){
-  this.searchText = $event
+      if (aValue < bValue) return direction === 'asc' ? -1 : 1;
+      if (aValue > bValue) return direction === 'asc' ? 1 : -1;
+      return 0;
+    });
+  }
 
-}
-getLayoutFlag(event:any){
-console.log(event)
-this.isLayout = event=='card' ? 'table':'card'
-// if(event){
-// this.isLayout = 'card'
-// }else{
-// this.isLayout = 'table'
-// }
-}
+  editGodown(godown: GodownDetails) {}
 }
--- src/app/pages/shared/filter-header/filter-header.component.ts
+++ src/app/pages/shared/filter-header/filter-header.component.ts
@@ -11,10 +11,11 @@ import { CommonService } from '../../../services/common.service';
 import { DynamicFormComponent } from '../dynamic-form/dynamic-form.component';
 import { formConfig } from '../config';
 import { IForm } from '../../../shared/interfaces/dynamic-form/form.interface';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'filter-header',
-  imports: [FormsModule, DynamicFormComponent],
+  imports: [FormsModule, DynamicFormComponent,JsonPipe],
   templateUrl: './filter-header.component.html',
   styleUrl: './filter-header.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -27,9 +28,21 @@ export class FilterHeaderComponent implements OnInit {
   @Output() changeInput = new EventEmitter<string>();
   @Output() changeLayoutFlag = new EventEmitter<string>();
   @Input() formData = [];
+  @Input() addNewGodown!:IForm;
+
+  // Sorting vars
+  @Output() sortChange = new EventEmitter<{
+    key: string;
+    direction: string;
+  }>();
+  sortKey = '';
+  direction: 'asc' | 'desc' = 'asc';
+  @Input() sortOpt:any = '';
+  // Sorting Ends
+
   isFlag: string = '';
   setMessage: string = '';
-  addNewGodown = formConfig as IForm;
+
 
   constructor(public common: CommonService) {}
 
@@ -52,8 +65,36 @@ export class FilterHeaderComponent implements OnInit {
   }
 
   changeLayout() {
-    let value = this.isFlag ? 'card' : 'table';
-    this.common.layouFlag$.next(value);
-    this.changeLayoutFlag.emit(value);
+    if(this.isFlag=='card' || this.isFlag==''){
+     this.isFlag ='table'
+    }else{
+      this.isFlag ='card'
+    }
+    this.common.layouFlag$.next( this.isFlag);
+    this.changeLayoutFlag.emit( this.isFlag);
+  }
+
+  sort(Evtr: Event) {
+    const key = (Evtr.target as HTMLSelectElement);
+    let value = key.value; // value will be a JSON string
+    let option = JSON.parse(value); // parse back to object
+    this.sortChange.emit({ key: option.column, direction: option.direction });
+
+    // const selectedOption = this.sortOptions[(Evtr.target as HTMLSelectElement).selectedIndex];
+    // this.sortChange.emit({ key: selectedOption.column, direction: selectedOption.direction });
+    // const key = (Evtr.target as HTMLSelectElement).value;
+    // if (this.sortKey === key) {
+    //   // toggle direction if same key selected again
+    //   this.direction = this.direction === 'asc' ? 'desc' : 'asc';
+    // } else {
+    //   // new key → reset to ascending
+    //   this.sortKey = key;
+    //   this.direction = 'asc';
+    // }
+
+    // this.sortChange.emit({
+    //   key: this.sortKey,
+    //   direction: this.direction,
+    // });
   }
 }
